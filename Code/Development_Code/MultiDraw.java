50b269ea363819c4596631218af48d374e8451c1
JJTrapani <JJTrapani@85cbca98-1f2e-11de-9d52-39b120432c5d>
2009-04-26 17:37:29 +0000

    1)  Added two new files to the project to handle the new window. 2)  Removed applet handling in the MultiDraw.java file; it is no longer a requirement.  Also removed the Init() method.

--- Code/Development_Code/MultiDraw.java
+++ Code/Development_Code/MultiDraw.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.EventListener;
 import javax.swing.*;
+
 import java.net.*;
 
 public class MultiDraw extends JApplet  {
@@ -13,10 +14,12 @@ public class MultiDraw extends JApplet  {
 	private SessionManager sessionMgr;
 	protected DrawingCanvas canvas;
 	protected ControlPanelView controlPanel;
+	protected static InitialWindowView initialwindow;
 	protected ToolBarView toolBar;
 	protected MenuBarView menuBar;
 	protected ArrayList<ToolController> tools;
-	protected boolean isApplet = false;
+
+	
 	
 
   /* Constructors  */
@@ -24,78 +27,66 @@ public class MultiDraw extends JApplet  {
 	/**
 	 * Create a new instance of MultiDraw class.
 	 */
-	public MultiDraw(boolean isApplet) 
-	{ 
-		this.isApplet = isApplet;
-		if (!isApplet) 
-		{
-			init();
-		}
-	}
-  
-	/**
-	 *  Create a new instance of MultiDraw class.
-	 */
 	public MultiDraw() 
-	{
-		/* invoked as Applet */
-		this(true);
+	{ 
+		
+
+		
+		  sessionMgr = new SessionManager();
+		  
+		    getContentPane().setLayout(new BorderLayout());
+		    canvas = createDrawingCanvas();
+		    getContentPane().add(canvas, BorderLayout.CENTER);
+		    controlPanel = createControlPanelView();
+		    getContentPane().add(controlPanel, BorderLayout.SOUTH);
+		    tools = createTools();
+		    toolBar = createToolBarView(tools);
+		    getContentPane().add(toolBar, BorderLayout.WEST);
+		    menuBar = createMenuBarView(tools);
+		    getContentPane().add (menuBar, BorderLayout.NORTH);
+		    
+		    
+		    /**
+			   * TODO: REMOVE LATER
+			   * This is temp code, it automatically creates a new session,
+			   * so that Object draw functionality will work.
+			   */
+			  String tempName;
+		    if(isMaster)
+		    {
+		    	tempName = "Ben_Master";
+		    }
+		    else
+		    {
+		    	tempName = "Ben_Slave";
+		    }
+		    	Member temp = new Member(tempName);
+			  
+			  Session tempAuto;
+			  if (isMaster)
+			  {
+				  tempAuto = ServerUtils.buildSession(temp,canvas, tools);
+			  }
+			  else
+			  {
+				  tempAuto = ServerUtils.buildSession(temp, canvas, "128.255.19.62", 3000, tools);
+			  }
+			  sessionMgr.addNewSession(tempAuto);
+			  canvas.updateSession(tempAuto);
+			  controlPanel.updateSession(tempAuto);
+			  
+			  sessionMgr.start();
+
 	}
-  
-	/**
-	 * Initialize class.
-	 */
-	public void init() 
-	{
-	  sessionMgr = new SessionManager();
-	  
-    getContentPane().setLayout(new BorderLayout());
-    canvas = createDrawingCanvas();
-    getContentPane().add(canvas, BorderLayout.CENTER);
-    controlPanel = createControlPanelView();
-    getContentPane().add(controlPanel, BorderLayout.SOUTH);
-    tools = createTools();
-    toolBar = createToolBarView(tools);
-    getContentPane().add(toolBar, BorderLayout.WEST);
-    menuBar = createMenuBarView(tools);
-    getContentPane().add (menuBar, BorderLayout.NORTH);
-    
-    
-    /**
-	   * TODO: REMOVE LATER
-	   * This is temp code, it automatically creates a new session,
-	   * so that Object draw functionality will work.
-	   */
-	  String tempName;
-    if(isMaster)
-    {
-    	tempName = "Ben_Master";
-    }
-    else
-    {
-    	tempName = "Ben_Slave";
-    }
-    	Member temp = new Member(tempName);
-	  
-	  Session tempAuto;
-	  if (isMaster)
-	  {
-		  tempAuto = ServerUtils.buildSession(temp,canvas, tools);
-	  }
-	  else
-	  {
-		  tempAuto = ServerUtils.buildSession(temp, canvas, "192.168.1.3", 3000, tools);
-	  }
-	  sessionMgr.addNewSession(tempAuto);
-	  canvas.updateSession(tempAuto);
-	  controlPanel.updateSession(tempAuto);
-	  
-	  sessionMgr.start();
-  }
+
 
 	
 	/* Factory methods  */
-  
+ 
+	protected static InitialWindowView createInitialWindowView() {
+		return new InitialWindowView();
+	}	
+	
 	protected DrawingCanvas createDrawingCanvas() {
 		return new DrawingCanvas();
 	}
@@ -119,7 +110,7 @@ public class MultiDraw extends JApplet  {
 	    ArrayList<ToolController> actions = new ArrayList();
 	
 	    actions.add(
-	                new ToolController("Freehand",
+	        new ToolController("Freehand",
 	  	        getImageIcon("freehand.jpg"),
 	  	        "freehand drawing tool",
 	  	        canvas,
@@ -171,44 +162,27 @@ public class MultiDraw extends JApplet  {
   }
   
   protected ImageIcon getImageIcon(String fileName) {
-    if(isApplet) 
-    {
-      try {
-        URL url = new URL(getCodeBase(), fileName);
-        return new ImageIcon(url);
-      }
-      catch(MalformedURLException e) {
-        return null;
-      }
-    }
-    else {
       return new ImageIcon(fileName);
-    }
   }
-
-  /**
+  
+  
+    
+    /**
    * Create a new instance of MultiDraw, as an application.
    * @param args Not used.
    */
   public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    frame.setTitle("MultiDraw Development Build");
-    frame.getContentPane().setLayout(new BorderLayout());
-    frame.getContentPane().add(new MultiDraw(false),
-			      BorderLayout.CENTER);
-    frame.addWindowListener(new AppCloser());
-    frame.pack();
-    frame.setSize(600, 600);
-    frame.setVisible(true);
+    
+	  initialwindow = createInitialWindowView();
+
   }
 
   
-  /* Inner class AppCLoser for terminating application  */
-  /* when Close Window button of frame is clicked       */
-  static class AppCloser extends WindowAdapter  {
-    public void windowClosing(WindowEvent e) {
-       System.exit(0);
-    }
-  }
-  
 }
+
+  
+  
+  
+  
+  
+  
